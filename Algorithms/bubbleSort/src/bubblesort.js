d9f110589c57d5e7db1c5edf5e5cc17d1b1f6a55
Nour Mo <nora008@student.kristiania.no>
2025-06-08 11:50:06 -0400

    Add files via upload
    Bubblesort algo

---
+++ Algorithms/bubbleSort/src/bubblesort.js
@@ -0,0 +1,15 @@
+function bubbleSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - i - 1; j++) {
+            if (array[j] < array[j + 1]) {
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+array = bubbleSort([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+console.log(array);
